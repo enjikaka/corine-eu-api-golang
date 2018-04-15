74d66dc37da4a2da3634f41e7bab8da7e6899f4c
Jeremy Karlsson <karlsson@jeremy.se>
2018-04-15 12:15:00 +0200

    Use copernicus package and set up primitive http handler

--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
 
-func main() {
-	fmt.Println("hello world")
+	"github.com/enjikaka/copernicus"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s", copernicus.Test())
 }
 
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
